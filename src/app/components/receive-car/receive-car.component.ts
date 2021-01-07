2a7bfce6fcb0fc4f6e3e82e2d473b5df9a42b8e0
Zushe <chaimzushe@gmail.com>
2021-01-06 20:55:14 -0500

    added bones to receive car

--- src/app/components/receive-car/receive-car.component.ts
+++ src/app/components/receive-car/receive-car.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { subNavInfo } from 'src/app/models/car.model';
 import { CarService } from 'src/app/services/car.service';
 import { RepairService } from 'src/app/services/repair.service';
@@ -17,16 +18,46 @@ export class ReceiveCarComponent implements OnInit {
   allCars = [];
   allRepairs = [];
   selectedCar = null;
+  selectedRepair = null;
   filteredCars = [];
-  constructor(private carService: CarService, private repairService: RepairService) { }
+  repairsNeeded = [];
+  filteredRepairs = [];
+  constructor(private carService: CarService, private repairService: RepairService, private snackbar: MatSnackBar) { }
 
   async ngOnInit() {
     this.filteredCars = this.allCars = await this.carService.getAllCars().toPromise() as [];
-    this.allRepairs = await this.repairService.getAllRepairs().toPromise() as [];
+    this.filteredRepairs = this.allRepairs = await this.repairService.getAllRepairs().toPromise() as [];
   }
 
-  filterCars(e){
-    this.filteredCars = this.allCars.filter( c => String(c.carNumber).includes(e.currentTarget.value));
+  filterCars(e) {
+    this.filteredCars = this.allCars.filter(c => String(c.carNumber).includes(e.currentTarget.value));
+  }
+
+  filterRepairs(e) {
+    this.filteredRepairs = this.allRepairs.filter(c => {
+      return c.name.includes(e.currentTarget.value) && !this.alreadySelected(c);
+    });
+  }
+
+  addRepair(event) {
+    setTimeout(x => {
+      this.selectedRepair = null;
+      this.filteredRepairs = this.allRepairs.filter(c => !this.alreadySelected(c));
+    }, 100)
+
+    this.repairsNeeded.push({ name: event.option.value })
+
+  }
+  removeRepair(i){
+    this.repairsNeeded.splice(i, 1);
+    this.filteredRepairs = this.allRepairs.filter(c => !this.alreadySelected(c));
+  }
+  alreadySelected(r) {
+    return this.repairsNeeded.find(rep => rep.name === r.name);
+  }
+
+  createLabel(){
+    return this.snackbar.open("Work in progress. Thank you for your patience 😊 ", "dismiss", { duration: 3000, panelClass: "err-panel" })
   }
 
 }
