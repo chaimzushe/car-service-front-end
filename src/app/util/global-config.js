e51de6277ffbef4239b03d7d78a96893a6ea49ff
Zushe <chaimzushe@gmail.com>
2021-01-14 21:59:43 -0500

    fixed redirect"

--- src/app/util/global-config.js
+++ src/app/util/global-config.js
@@ -1,4 +1,6 @@
 export const baseApi = "https://car-service-shop.herokuapp.com/api";
 //export const baseApi = "http://localhost:2000/api";
 
-export const redirectUrl = "http://localhost:4200/";
+//export const redirectUrl = "http://localhost:4200/";
+export const redirectUrl = "https://183empire.netlify.app/";
+
