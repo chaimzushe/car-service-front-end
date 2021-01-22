242335e9bda594e65792aa7f15a8975121796e64
Zushe <chaimzushe@gmail.com>
2021-01-22 07:41:37 -0500

    added ful story

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from './services/auth.service';
-
+import * as FullStory from '@fullstory/browser';
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private authService: AuthService, private snackbar: MatSnackBar){
-
+  user: any;
+  constructor(private authService: AuthService, private snackbar: MatSnackBar) {
+    FullStory.init({
+      orgId: '10KCHC',
+      devMode: !environment.production
+    });
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.authService.retrieveAuthFromUrl();
-    if(window.screen.width < 650 && this.showFooter()){
+    if (window.screen.width < 650 && this.showFooter()) {
       let msg = "Looks like you are using a mobile device. This site is not optimized for small screens";
       return this.snackbar.open(msg, "dismiss", { duration: 10000, panelClass: "err-panel" })
     }
+    let userSub = this.authService.userSubject.subscribe(user => {
+      if (user) {
+        this.user = user;
+        FullStory.identify(user._id);
+        FullStory.setUserVars({
+          name: user.name,
+          email: user.email
+        });
+
+      }
+    })
+    if (this.user) userSub.unsubscribe()
   }
-  showFooter(){
+  showFooter() {
     return !window.location.href.includes('entry-page')
   }
 }
