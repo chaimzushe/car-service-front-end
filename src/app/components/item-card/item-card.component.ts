351451d85c1cce1e88d7c1c30f0d76bcad2648d8
Zushe <chaimzushe@gmail.com>
2021-01-28 21:24:51 -0500

    small stayle changes

--- src/app/components/item-card/item-card.component.ts
+++ src/app/components/item-card/item-card.component.ts
@@ -1,19 +1,33 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.scss'],
 })
-export class ItemCardComponent implements OnInit {
+export class ItemCardComponent implements OnInit, AfterViewInit {
+  @ViewChild('host', { static: false }) host: ElementRef;
   @Input() index;
   @Input() item;
   @Output() edited = new EventEmitter();
   @Output() deleted = new EventEmitter();
-  constructor() {}
+  highlight = false;
+  constructor() { }
 
   ngOnInit(): void {
 
+  }
+  ngAfterViewInit() {
+    if (this.item.highLite) this.animateAndScroll()
+  }
+
+  animateAndScroll() {
+    this.highlight = true;
+    this.host.nativeElement.scrollIntoView(true);
+    setTimeout(() => {
+      this.highlight = false;
+      this.item.highLite = false;
+    }, 3000);
   }
 
   navigateToEdit() {
